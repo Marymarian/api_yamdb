57e6a9cdbf23ed77395f6bcfa981e32f65535f6a
kotyancutetyan <108882164+kotyancutetyan@users.noreply.github.com>
2022-11-29 23:55:47 +0500

    Созданы модели комментариев и рейтинга

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-from .validators import validate_year
+# from .validators import validate_year
 
 
 class Reviews(models.Model):
