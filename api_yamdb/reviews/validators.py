00c2935518fbd194c24892c2bfc7b270d2495290
Марина Вторушина <ms13vmv@gmail.com>
2022-11-29 18:53:41 +0300

    Модели произведений, жанров и категорий и модель принадлежности готовы.

---
+++ api_yamdb/reviews/validators.py
@@ -0,0 +1,11 @@
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+
+def validate_for_year(value):
+    """Дата выхода не может быть позднее текущего года."""
+    if value > timezone.now().year:
+        raise ValidationError(
+            ('%(value)s год позднее текущего года!'),
+            params={'value': value},
+        )
